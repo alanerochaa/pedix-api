2fad5f4761cdfbf727a89fd364b0b608f7c3e659
alanerochaa <alanerochaa@gmail.com>
2025-10-12 00:36:06 -0300

    Ajustes finais.

--- src/main/java/com/pedix/api/controller/HomeController.java
+++ src/main/java/com/pedix/api/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(value = "/home", produces = MediaType.TEXT_HTML_VALUE)
     public String home() {
         return """
                 <!DOCTYPE html>
